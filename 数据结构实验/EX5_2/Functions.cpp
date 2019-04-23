3e2dd478d60e705d6f4b3e23812eef9c809ebede
ypmonitor <778116674@qq.com>
2019-04-23 23:35:14 +0800

    add Ex5_2-----LinkStack

---
+++ 数据结构实验/EX5_2/Functions.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include "EX5_2.h"
+
+
+void InitStack(LinkStack &S )
+{
+	S=NULL;
+}
+
+Status StackEmpty(LinkStack S)
+{   
+	if (S==NULL) return true;
+	else return false;
+}
+
+Status Push(LinkStack &S , SElemType e)
+{
+	LinkStack p;
+	p=new StackNode;      //生成新结点p
+	if (!p) exit(OVERFLOW);
+	p->data=e; 
+	p->next=S; S=p; 
+	return OK; 
+}
+
+Status Pop (LinkStack &S,SElemType &e)
+{
+	LinkStack p;
+	if (S==NULL) 
+		return ERROR;
+	e = S-> data; 
+	p = S;  
+	S =  S-> next;
+	delete p; 
+	return OK;  
+}  
+
+
+SElemType GetTop(LinkStack S)
+{
+	if (S==NULL) 
+		exit(1);
+	else 
+		return 	S->data;
+}
+
+void DestroyStack(LinkStack &S)
+{
+	if(S==NULL) return;
+	LinkStack p;
+	while(S){
+		p = S;
+		S = S->next;
+		delete p;
+	}
+}
